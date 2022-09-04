2f93233c10b014e2b3dd6b69f1e54e8f6d1e6582
Yasser8351 <yasser8351@gmail.com>
2022-09-04 09:44:11 +0200

    fix stop playing when close the app

--- lib/screen/play_music.dart
+++ lib/screen/play_music.dart
@@ -23,9 +23,11 @@ class _PlayMusicState extends State<PlayMusic> {
   int index = 0;
   String name = 'no data';
   int expandedIndex = -1;
+  DateTime timeBackPressed = DateTime.now();
   List<MusicModel> mylist = [
     MusicModel(name: "سورة يس", url: 'yaseen.mp3'),
     MusicModel(name: "سورة الفرقان", url: 'alforgan.mp3'),
+    MusicModel(name: "سورة يس", url: 'yaseen.mp3'),
   ];
   @override
   void initState() {
@@ -37,6 +39,19 @@ class _PlayMusicState extends State<PlayMusic> {
     setUp();
     //getCurrentPostion();
   }
+
+  // @override
+  // void didChangeAppLifecycleState(AppLifecycleState state) {
+  //   log("state Screen : $state");
+  //   if (state == AppLifecycleState.resumed) {
+  //     // Audio player is a custom class with resume and pause static methods
+  //     playMusic("");
+  //     log("state Screen : $state");
+  //   } else {
+  //     log("state Screen : $state");
+  //     stopMusic();
+  //   }
+  // }
 
   setUp() {
     player.onAudioPositionChanged.listen((d) {
@@ -76,186 +91,227 @@ class _PlayMusicState extends State<PlayMusic> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 0, 58, 53),
-        title: const Text(
-          "القران الكريم كامل بصوت الشيخ محمد عثمان",
-          textAlign: TextAlign.right,
-          style: TextStyle(fontSize: 15),
-        ),
-      ),
-      bottomNavigationBar: Builder(builder: (context) {
-        if (expandedIndex == -1) {
-          return const SizedBox();
-        } else {
-          return Container(
-            color: Colors.white,
-            height: 130,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Align(
-                      alignment: Alignment.centerRight, child: Text(name)),
+    return Stack(children: [
+      Align(
+          alignment: Alignment.topCenter,
+          child: WillPopScope(
+            onWillPop: () async {
+              setState(() => isPlaying = false);
+              stopMusic();
+              return true;
+              // final differenc = DateTime.now().difference(timeBackPressed);
+              // final exitApp = differenc >= const Duration(seconds: 2);
+
+              // timeBackPressed = DateTime.now();
+
+              // if (exitApp) {
+              //   //setState(() => isPlaying = false);
+              //   //stopMusic();
+              //   ScaffoldMessenger.of(context).showSnackBar(
+              //     SnackBar(
+              //       backgroundColor: Theme.of(context).primaryColor,
+              //       duration: const Duration(seconds: 2),
+              //       content: const Text(
+              //         "اضغط مرة اخرى للخروج",
+              //         textAlign: TextAlign.right,
+              //       ),
+              //     ),
+              //   );
+              //   return false;
+              // } else {
+              //   return true;
+              // }
+            },
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: const Color.fromARGB(255, 0, 58, 53),
+                title: const Text(
+                  "القران الكريم كامل بصوت الشيخ محمد عثمان",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontSize: 15),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(formatTimeCurrentPostion(currentPostion)),
-                    //Text('${currentPostion.inSeconds.toDouble()}'),
-                    SizedBox(
-                        width: 250,
-                        child: Slider(
-                            thumbColor: const Color(0xff001614),
-                            inactiveColor:
-                                const Color.fromARGB(255, 2, 145, 133),
-                            activeColor: const Color.fromARGB(255, 4, 61, 56),
-                            value: currentPostion.inSeconds.toDouble(),
-                            max: musicLength.inSeconds.toDouble(),
-                            onChanged: (val) {
-                              seekTo(val.toInt());
-                            })),
-                    Text(formatTimeEndPostion(musicLength)),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.first_page),
-                      iconSize: 35,
-                      onPressed: () {
-                        if (index > 0) {
-                          setState(() {
-                            index--;
-                            isPlaying = true;
-                            print('$index');
-                          });
-                          cache.play(mylist[index].url);
-                        } else {
-                          setState(() {
-                            isPlaying = true;
-                          });
-                          print('$index');
-                          cache.play(mylist[index].url);
-                        }
-                      },
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        if (isPlaying) {
-                          setState(() {
-                            isPlaying = false;
-                          });
-                          stopMusic();
-                        } else {
-                          setState(() {
-                            isPlaying = true;
-                          });
-                          playMusic(mylist[index].url);
-                        }
-                      },
-                      icon: isPlaying
-                          ? const Icon(Icons.pause)
-                          : const Icon(Icons.play_arrow),
-                      iconSize: 35,
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.last_page),
-                      iconSize: 35,
-                      onPressed: () {
-                        if (index < mylist.length - 1) {
-                          setState(() {
-                            index = index + 1;
-                            isPlaying = true;
-                          });
-                          log(' next $index');
-                          cache.play(mylist[index].url);
-                        } else {
-                          setState(() {
-                            index = 0;
-                            isPlaying = true;
-                          });
-                          log(" back $index");
-                          cache.play(mylist[index].url);
-                        }
-                      },
-                    )
-                  ],
-                ),
-              ],
-            ),
-          );
-        }
-      }),
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: const BoxDecoration(
-            color: Color(0xff001614),
-            image: DecorationImage(
-              image: AssetImage("assets/image.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          //color: const Color(0xff001614),
-          height: size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(height: size.height * .01),
-              Container(
-                height: size.height * .8,
-                width: double.infinity,
-                alignment: Alignment.center,
-                child: ListView.builder(
-                  itemCount: mylist.length,
-                  itemBuilder: (context, index) {
-                    // return ItemListView(model: mylist[index]);
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          name = mylist[index].name;
-                          expandedIndex = index;
-                          isPlaying = true;
-                          playMusic(mylist[index].url);
-                        });
-                      },
-                      //expandedIndex
-                      child: Container(
-                        color: expandedIndex == index
-                            ? Colors.white
-                            : const Color(0xff001614),
-                        child: ListTile(
-                          trailing: Text(
-                            // " ${mylist[index].name}  ${index + 1}",
-                            " ${mylist[index].name}",
-                            //"$name ${index + 1}",
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              color: expandedIndex != index
-                                  ? Colors.white
-                                  : const Color(0xff001614),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                //child: Image.asset("assets/logo2.jpg"),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+              bottomNavigationBar: Builder(builder: (context) {
+                if (expandedIndex == -1) {
+                  return const SizedBox();
+                } else {
+                  return Container(
+                    color: Colors.white,
+                    height: 130,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(name)),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(formatTimeCurrentPostion(currentPostion)),
+                            //Text('${currentPostion.inSeconds.toDouble()}'),
+                            SizedBox(
+                                width: 250,
+                                child: Slider(
+                                    thumbColor: const Color(0xff001614),
+                                    inactiveColor:
+                                        const Color.fromARGB(255, 2, 145, 133),
+                                    activeColor:
+                                        const Color.fromARGB(255, 4, 61, 56),
+                                    value: currentPostion.inSeconds.toDouble(),
+                                    max: musicLength.inSeconds.toDouble(),
+                                    onChanged: (val) {
+                                      seekTo(val.toInt());
+                                    })),
+                            Text(formatTimeEndPostion(musicLength)),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.first_page),
+                              iconSize: 35,
+                              onPressed: () {
+                                if (index > 0) {
+                                  setState(() {
+                                    index--;
+                                    expandedIndex = index;
+                                    name = mylist[index].name;
+                                    isPlaying = true;
+                                    print('$index');
+                                  });
+                                  cache.play(mylist[index].url);
+                                } else {
+                                  setState(() {
+                                    expandedIndex = index;
+                                    name = mylist[index].name;
+                                    isPlaying = true;
+                                  });
+                                  print('$index');
+                                  cache.play(mylist[index].url);
+                                }
+                              },
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                if (isPlaying) {
+                                  setState(() {
+                                    isPlaying = false;
+                                  });
+                                  stopMusic();
+                                } else {
+                                  setState(() {
+                                    isPlaying = true;
+                                  });
+                                  playMusic(mylist[index].url);
+                                }
+                              },
+                              icon: isPlaying
+                                  ? const Icon(Icons.pause)
+                                  : const Icon(Icons.play_arrow),
+                              iconSize: 35,
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.last_page),
+                              iconSize: 35,
+                              onPressed: () {
+                                if (index < mylist.length - 1) {
+                                  setState(() {
+                                    index = index + 1;
+                                    expandedIndex = index;
+                                    name = mylist[index].name;
+                                    isPlaying = true;
+                                    log(' next index $index');
+                                  });
+                                  cache.play(mylist[index].url);
+                                } else {
+                                  setState(() {
+                                    index = 0;
+                                    expandedIndex = index;
+                                    name = mylist[index].name;
+                                    isPlaying = true;
+                                    log(" back index $index");
+                                  });
+                                  cache.play(mylist[index].url);
+                                }
+                              },
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              }),
+              body: SingleChildScrollView(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xff001614),
+                    image: DecorationImage(
+                      image: AssetImage("assets/image.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  //color: const Color(0xff001614),
+                  height: size.height,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(height: size.height * .01),
+                      Container(
+                        height: size.height * .8,
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        child: ListView.builder(
+                          itemCount: mylist.length,
+                          itemBuilder: (context, index) {
+                            // return ItemListView(model: mylist[index]);
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  log("on Tap");
+                                  name = mylist[index].name;
+                                  expandedIndex = index;
+                                  isPlaying = true;
+                                  playMusic(mylist[index].url);
+                                });
+                              },
+                              //expandedIndex
+                              child: Container(
+                                color: expandedIndex == index
+                                    ? Colors.white
+                                    : null,
+                                child: ListTile(
+                                  trailing: Text(
+                                    " ${mylist[index].name}",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      color: expandedIndex != index
+                                          ? Colors.white
+                                          : const Color(0xff001614),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        //child: Image.asset("assets/logo2.jpg"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ))
+    ]);
   }
 
   playMusic(String song) {
     // to play the Audio
-
     cache.play(song);
   }
 
@@ -272,6 +328,16 @@ class _PlayMusicState extends State<PlayMusic> {
   @override
   void dispose() {
     super.dispose();
+    stopMusic();
     player.dispose();
+    log("dispose");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    stopMusic();
+    player.dispose();
+    log("deactivate");
   }
 }
